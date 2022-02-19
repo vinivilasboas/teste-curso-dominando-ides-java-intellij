82793f00ca4912ce0e496936901b1c13660fe7ec
vinivilasboas <vinivilasboas@gmail.com>
2022-02-19 15:49:51 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Olá, Mundo! " + (a + b));*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+
+}
